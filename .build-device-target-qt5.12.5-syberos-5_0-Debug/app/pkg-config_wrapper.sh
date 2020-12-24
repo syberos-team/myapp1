e3b7ce2fdbe2fda02d575e9c9d9c64a9b2f6f1d2
Xue Jun <xuejun@syberos.com>
2020-12-24 09:58:22 +0800

    chore: 删除编译目录

--- .build-device-target-qt5.12.5-syberos-5_0-Debug/app/pkg-config_wrapper.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-PKG_CONFIG_SYSROOT_DIR=/home/xuejun/SyberOS-CCE/SyberOS5_0/sysroot
-export PKG_CONFIG_SYSROOT_DIR
-PKG_CONFIG_LIBDIR=/home/xuejun/SyberOS-CCE/SyberOS5_0/sysroot/usr/lib64/pkgconfig
-export PKG_CONFIG_LIBDIR
-exec pkg-config "$@"
